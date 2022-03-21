2bfca1259c9f62c80cc55037ca7ede7f462a5582
Bianca Crișu <100210311+crisubianca@users.noreply.github.com>
2022-03-21 21:01:56 +0200

    Create HandleFiles.java

---
+++ Tema2/Main/src/tema2/HandleFiles.java
@@ -0,0 +1,48 @@
+package tema2;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class HandleFiles {
+
+    public HandleFiles() {
+
+    }
+
+    public void zipFile(String sourceFile, FileOutputStream fos) throws IOException {
+        ZipOutputStream zipOut = new ZipOutputStream(fos);
+        File fileToZip = new File(sourceFile);
+
+        FileInputStream fis = new FileInputStream(fileToZip);
+        ZipEntry zipEntry = new ZipEntry(fileToZip.getName());
+        zipOut.putNextEntry(zipEntry);
+        byte[] bytes = new byte[1024];
+        int length;
+        while ((length = fis.read(bytes)) >= 0) {
+            zipOut.write(bytes, 0, length);
+        }
+        zipOut.close();
+        fis.close();
+        fos.close();
+    }
+
+    public long getSize(String filePath){
+        Path path = Paths.get(filePath);
+        long bytes = 0;
+        try {
+            bytes = Files.size(path);
+        } catch (IOException e) {
+            System.out.println("Couldn't display file size.");
+        }
+        return bytes;
+    }
+
+    public String sizeToString(String filePath, long fileSize){
+        String s = "";
+        s = s + String.valueOf(fileSize) + " bytes for " + filePath;
+        return s;
+    }
+}
