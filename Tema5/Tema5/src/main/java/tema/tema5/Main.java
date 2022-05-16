9d52005bc92aa160c0584d6a735245d3b0d9d926
Bianca Crișu <100210311+crisubianca@users.noreply.github.com>
2022-05-16 10:25:35 +0300

    Create Main.java

---
+++ Tema5/Tema5/src/main/java/tema/tema5/Main.java
@@ -0,0 +1,16 @@
+package tema.tema5;
+
+import java.security.NoSuchAlgorithmException;
+import static tema.tema5.Hash.getSHA;
+import static tema.tema5.Hash.toHexString;
+import tema.tema5.Hash;
+
+/**
+ *
+ * @author Adm
+ */
+public class Main {
+    Hash sha256 = new Hash();
+    
+    
+}
